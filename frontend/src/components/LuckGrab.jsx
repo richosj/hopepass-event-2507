4fcd7919b822535549b5cea9316805b0ff0964d8
richosj <44463951+richosj@users.noreply.github.com>
2025-07-21 00:11:50 +0900

    setting ignore

--- frontend/src/components/LuckGrab.jsx
+++ frontend/src/components/LuckGrab.jsx
@@ -3,10 +3,23 @@ import './LuckGrab.scss';
 
 const LuckGrab = () => {
   return (
-    <div className="luck-grab">
-      <div className="luck-grab__inner">
-        <div className="luck-grab__title">
-          <h2>행운을 모으세요!</h2>
+    <div className="luck">
+      <div className="luck__inner">
+        <div className="luck__container">
+          <div className="luck__title">
+            <span><img src="/src/assets/lucky/title.png" alt="" /></span>
+          </div>
+
+          <div className="luck__step">
+            <img src="/src/assets/lucky/step.png" alt="" />
+            <div className="luck__step-text">
+              <p>
+                * 희망ON 드로우 희망 한마디, 희망 플러스 이벤트 참여자는<br />
+                기존에 전달 받은 난수번호로 바로 참여 가능합니다.
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
--- frontend/src/components/Visual.jsx
+++ frontend/src/components/Visual.jsx
@@ -5,8 +5,22 @@ const Visual = () => {
   return (
     <div className="visual">
       <div className="visual__inner">
-        <div className="visual__title">
-          
+        <div className="visual__container">
+          <div className='icon-01 icon-position'><i aria-label="닌텐도 이모티콘"><img src="/src/assets/visual/emoticon-1.png" alt="" /></i></div>
+          <div className='icon-02 icon-position'><i aria-label="편지 이모티콘"><img src="/src/assets/visual/emoticon-2.png" alt="" /></i></div>
+          <div className="visual__title">
+            <span className="off"><img src="/src/assets/visual/title-off.png" alt="" /></span>
+          </div>
+          <div className="visual__info">
+            <dl>
+              <dt>희망패스 미션 기간</dt>
+              <dd>2025.08.04 ~ 2025.08.31</dd>
+            </dl>
+            <dl>
+              <dt>희망패스 룰렛 이벤트 기간</dt>
+              <dd>2025.09.01 ~ 2025.09.30</dd>
+            </dl>
+          </div>
         </div>
       </div>
     </div>
