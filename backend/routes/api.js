fdb505fcfc0941df25860ab5d3707f6b5c11ada6
richosj <okm760596@gmail.com>
2025-11-20 18:41:36 +0900

    - 확률 변경

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require('../models/db')
 
 // 룰렛 확률
-const baseProb = { 1: 0, 2: 0.4, 3: 1, 4: 5, 5: 94.4 }
+const baseProb = { 1: 0, 2: 2.5, 3: 4, 4: 8, 5: 85.5 }
 const limits = { 1: 1, 2: 2, 3: 5, 4: 20 }
 
 const forceWinCodes = ['t3S8dOr192n6']
